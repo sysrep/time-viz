cb876cd0eb30123d90b7c9c952f2a4236828cea2
Hung-Han Chen <chenhungh@gmail.com>
2017-02-06 19:50:01 +0200

    Add new styles and present more data on the <Event /> component.

--- src/components/event/index.js
+++ src/components/event/index.js
@@ -2,16 +2,106 @@ import React from 'react'
 import './style.css';
 import { getTimeInPercentage } from '../../lib/getTimeInPercentage.js'
 
-export const Event = (props) => (
-  <div
-    className="eventPoint"
-    style={{
-      left: `${getTimeInPercentage(props.event.start)}%`,
-    }}
-  >
-  </div>
-)
+export const Event = (props) => {
+  const { joy, surprise, fear, anger, sadness } = props.emotion;
+  const { sentiment } = props;
+  const positionScalar = 95;
+  const negativeness = (1 - sentiment) * positionScalar;
+  const heightScalar = props.zoom;
+  return (
+    <div
+      className="eventPointContainer"
+      style={{
+        left: `${getTimeInPercentage(props.event.start)}%`,
+      }}
+    >
+        <div
+          className="barContainer"
+          style={{
+            opacity: `${sentiment*0.85}`,
+          }}
+        >
+          <div
+            className="barShadow"
+            style={{
+              top: `${negativeness}%`,
+              height: `${(anger+(anger+joy)/2+joy+(joy+fear)/2+fear+surprise+sadness+(sadness+anger)/2)*heightScalar}%`,
+            }}
+          >
+          </div>
+          <div
+            className="anger"
+            style={{
+              top: `${negativeness}%`,
+              height: `${anger*heightScalar}%`,
+            }}
+          >
+          </div>
+
+          <div
+            className="anticipation"
+            style={{
+              top: `${negativeness+anger*heightScalar}%`,
+              height: `${(anger+joy)/2*heightScalar}%`,
+            }}
+          >
+          </div>
+          <div
+            className="joy"
+            style={{
+              top: `${negativeness+anger*heightScalar+(anger+joy)/2*heightScalar}%`,
+              height: `${joy*heightScalar}%`,
+            }}
+          >
+          </div>
+          <div
+            className="trust"
+            style={{
+              top: `${negativeness+anger*heightScalar+(anger+joy)/2*heightScalar+joy*heightScalar}%`,
+              height: `${(joy+fear)/2*heightScalar}%`,
+            }}
+          >
+          </div>
+          <div
+            className="fear"
+            style={{
+              top: `${negativeness+anger*heightScalar+(anger+joy)/2*heightScalar+joy*heightScalar+(joy+fear)/2*heightScalar}%`,
+              height: `${fear*heightScalar}%`,
+            }}
+          >
+          </div>
+          <div
+            className="surprise"
+            style={{
+              top: `${negativeness+anger*heightScalar+(anger+joy)/2*heightScalar+joy*heightScalar+(joy+fear)/2*heightScalar+fear*heightScalar}%`,
+              height: `${surprise*heightScalar}%`,
+            }}
+          >
+          </div>
+          <div
+            className="sadness"
+            style={{
+              top: `${negativeness+anger*heightScalar+(anger+joy)/2*heightScalar+joy*heightScalar+(joy+fear)/2*heightScalar+fear*heightScalar+surprise*heightScalar}%`,
+              height: `${sadness*heightScalar}%`,
+            }}
+          >
+          </div>
+          <div
+            className="disgust"
+            style={{
+              top: `${negativeness+anger*heightScalar+(anger+joy)/2*heightScalar+joy*heightScalar+(joy+fear)/2*heightScalar+fear*heightScalar+surprise*heightScalar+sadness*heightScalar}%`,
+              height: `${(sadness+anger)/2*heightScalar}%`,
+            }}
+          >
+          </div>
+        </div>
+    </div>
+  )
+}
 
 Event.propTypes = {
   event: React.PropTypes.object,
+  sentiment: React.PropTypes.number,
+  emotion: React.PropTypes.object,
+  zoom: React.PropTypes.number,
 }
