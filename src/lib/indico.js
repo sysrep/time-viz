9f236b4dae797dc696ab31da7a4ad02d302682bf
Hung-Han Chen <chenhungh@gmail.com>
2017-02-14 00:14:38 +0200

    Remove api_key

--- src/lib/indico.js
+++ src/lib/indico.js
@@ -1,9 +1,9 @@
 import $ from 'jquery';
 export const sentiment = (textArray) => {
   return $.post(
-    'https://apiv2.indico.io/sentiment/batch',
+    'https://apiv2.indico.io/sentimenthq/batch',
     JSON.stringify({
-      'api_key': "388cb7cf494d7dc12a3c83d2cb454772",
+      'api_key': "sss",
       'data': textArray,
     })
   )
@@ -13,7 +13,7 @@ export const emotion = (textArray) => {
   return $.post(
     'https://apiv2.indico.io/emotion/batch',
     JSON.stringify({
-      'api_key': "388cb7cf494d7dc12a3c83d2cb454772",
+      'api_key': "sss",
       'data': textArray,
     })
   )
