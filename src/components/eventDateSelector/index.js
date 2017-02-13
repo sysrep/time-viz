7f922c2bcbe8c22530c645bce958d1224df9ccf7
Hung-Han Chen <chenhungh@gmail.com>
2017-02-14 00:14:29 +0200

    Improve eventDateSelector component

--- src/components/eventDateSelector/index.js
+++ src/components/eventDateSelector/index.js
@@ -1,25 +1,55 @@
 import React from 'react'
+import './style.css';
+
+function pad(number) {
+  if (number < 10) {
+    return '0' + number;
+  }
+  return number;
+}
+
+export const getDate = (date) => {
+  return date.getFullYear() +
+    '-' + pad(date.getMonth() + 1) +
+    '-' + pad(date.getDate());
+}
+
+export const getTime = (date) => {
+  return pad(date.getHours()) +
+    ':' + pad(date.getMinutes()) +
+    ':' + pad(date.getSeconds());
+}
 
 export const EventDateSelector = (props) => (
-  <form>
-    <label>from
-      <input
-        type="datetime-local"
-        value={props.from}
-        onChange={props.onFromChange} />
-    </label>
-    <label>to
-      <input
-        type="datetime-local"
-        value={props.to}
-        onChange={props.onToChange} />
-    </label>
-  </form>
+  <div className="eventDateSelectorContainer">
+    <form>
+      <label>from
+        <input
+          type="date"
+          value={getDate(props.from)}
+          onChange={props.onFromChange} />
+        <input
+          type="time"
+          value={getTime(props.from)}
+          onChange={props.onFromChange} />
+      </label>
+      <label>to
+        <input
+          type="date"
+          value={getDate(props.to)}
+          onChange={props.onToChange} />
+        <input
+          type="time"
+          value={getTime(props.to)}
+          onChange={props.onToChange} />
+      </label>
+    </form>
+  </div>
 )
 
 EventDateSelector.propTypes = {
-  from: React.PropTypes.string,
-  to: React.PropTypes.string,
+  from: React.PropTypes.object,
+  to: React.PropTypes.object,
   onFromChange: React.PropTypes.func,
   onToChange: React.PropTypes.func,
 }
